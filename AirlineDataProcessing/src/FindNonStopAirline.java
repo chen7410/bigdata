fb4ca09683f695acd7737c40de0dbad4874df92c
Minqing <chen7410@uw.edu>
2019-06-18 14:13:08 -0700

    finish finding the non-stop airline

--- AirlineDataProcessing/src/FindNonStopAirline.java
+++ AirlineDataProcessing/src/FindNonStopAirline.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -9,32 +10,14 @@ import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 
 
-public class FindNonStopAirline {
-
-	public static void main(String[] args) throws Exception {
-		Configuration conf = new Configuration();
-		Job job = Job.getInstance(conf, "Find airport");
-		job.setJarByClass(FindAirportInCountry.class);
-		MultipleInputs.addInputPath(job, new Path(args[0]), 
-				TextInputFormat.class, RoutesMapper.class);
-		MultipleInputs.addInputPath(job, new Path(args[1]), 
-				TextInputFormat.class, FinalAirlinesMapper.class);
-		FileOutputFormat.setOutputPath(job, new Path(args[2]));
-		job.setReducerClass(FindNonStopAirlineReducer.class);
-		
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(Text.class);
-		
-		job.setInputFormatClass(TextInputFormat.class);
-		job.setOutputFormatClass(TextOutputFormat.class);
-		
-		System.exit(job.waitForCompletion(true) ? 0 : 1);
-	}
+public class FindNonStopAirline extends Configured implements Tool {
 	
 	/**
 	 * process the routes data.
@@ -90,15 +73,47 @@ public class FindNonStopAirline {
 	 */
 	public static class FindNonStopAirlineReducer extends Reducer<Text, Text, Text, Text> {
 		private Text dummyKey = new Text();
+		private String STOPS = "0";
+		private Text airlineName = new Text();
+		
 		/**
 		 * output the non-stop airline name.
 		 */
 		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+			boolean haveSTOPS  = false;
 			for (Text val : values) {
-				if (!val.equals("0")) {
-					context.write(dummyKey, val);
+				if (val.toString().length() > 1) {
+					airlineName.set(val);
+				}
+				if (val.toString().equals(STOPS)) {
+					haveSTOPS = true;
 				}
 			}
+			if (haveSTOPS && airlineName.toString().length() > 1) {
+				context.write(key, airlineName);
+			}
 		}
+	}
+
+	@Override
+	public int run(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		 Job job = Job.getInstance(conf, "FindNonStopAirline");
+		 job.setJarByClass(FindNonStopAirline.class);
+		 MultipleInputs.addInputPath(job,new Path(args[0]), TextInputFormat.class, RoutesMapper.class);
+		 MultipleInputs.addInputPath(job,new Path(args[1]), TextInputFormat.class, FinalAirlinesMapper.class);
+		  
+		 FileOutputFormat.setOutputPath(job, new Path(args[2]));
+		 job.setReducerClass(FindNonStopAirlineReducer.class);
+		 job.setNumReduceTasks(1);
+		 job.setOutputKeyClass(Text.class);
+		 job.setOutputValueClass(Text.class);
+		 
+		 return (job.waitForCompletion(true) ? 0 : 1);
+	}
+	
+	public static void main(String[] args) throws Exception {
+		int ecode = ToolRunner.run(new FindNonStopAirline(), args);
+		  System.exit(ecode);
 	}
 }
